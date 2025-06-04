8a8aa106ee211e36780f6f4d24ce992cb96dbb11
d3sti <83227941+d3stiii@users.noreply.github.com>
2025-06-05 00:06:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//     http_response_code(200);
+//     exit();
+// }
 
 function loadEnv($path = __DIR__ . '/.env')
 {
